bee5bdba2249c562e099d56b312a1855d3db7193
Pavani <vemasani.pavani@gmail.com>
2024-04-22 14:16:03 +0530

    code commented

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {}
-}
-//automatically download plugins relatively to cloud
-provider "aws" {
-  region = "us-east-1"
-}
-resource "null_resource" "resource" {}
+//terraform {
+//  backend "s3" {}
+//}
+////automatically download plugins relatively to cloud
+//provider "aws" {
+//  region = "us-east-1"
+//}
+//resource "null_resource" "resource" {}
