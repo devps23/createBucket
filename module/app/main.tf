cfbdc469a75d312545e5fa2c25cf667bf0ea1c10
Pavani <vemasani.pavani@gmail.com>
2024-04-23 18:44:17 +0530

    attach aws_iam_policy attach to the prometheus role

--- module/app/main.tf
+++ module/app/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+
   tags = {
     Name = var.component
   }
@@ -19,10 +20,17 @@ resource "aws_route53_record" "route" {
 resource "aws_iam_role" "Prometheus_role" {
   name               = "instance_role"
   assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
+
 }
 resource "aws_iam_instance_profile" "instance_profile" {
   name = var.role
   role = aws_iam_role.Prometheus_role.name
+
+}
+//attach policy to the prometheus roles
+resource "aws_iam_role_policy_attachment" "policy_attach" {
+  role       = aws_iam_role.Prometheus_role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
 //give the permissions with the help of policy
 resource "aws_iam_policy" "policy" {
