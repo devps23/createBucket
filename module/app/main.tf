722dd4d275b9d7cf5ff0f31ff5476974630baea5
Pavani <vemasani.pavani@gmail.com>
2024-04-25 18:50:56 +0530

    comment the code...

--- module/app/main.tf
+++ module/app/main.tf
@@ -2,14 +2,14 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  //iam_instance_profile = aws_iam_instance_profile.instance_profile.name
-  instance_market_options {
-    market_type="spot"
-    spot_options {
-      spot_instance_type = "persistent"
-      instance_interruption_behavior = "stop"
-    }
-  }
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+//  instance_market_options {
+//    market_type="spot"
+//    spot_options {
+//      spot_instance_type = "persistent"
+//      instance_interruption_behavior = "stop"
+//    }
+//  }
   tags = {
     Name = var.tool_name
     visual = "yes"
@@ -24,17 +24,17 @@ resource "aws_route53_record" "route" {
   ttl =40
 }
 
-//resource "aws_iam_role" "Prometheus_role" {
-//  name               = "prometheus_role"
-// assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
-//
-//}
-//resource "aws_iam_instance_profile" "instance_profile" {
-//  name = var.role
-//  role = aws_iam_role.Prometheus_role.name
-//
-//}
-////attach policy to the prometheus roles
+resource "aws_iam_role" "Prometheus_role" {
+  name               = "prometheus_role"
+ assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
+
+}
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = var.role
+  role = aws_iam_role.Prometheus_role.name
+
+}
+//attach policy to the prometheus roles
 //resource "aws_iam_role_policy_attachment" "policy_attach" {
 //  role       = aws_iam_role.Prometheus_role.name
 //  policy_arn = aws_iam_policy.policy.arn
