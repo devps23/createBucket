97150a4a4dd0fc00b36f1dbf799a3f35536627fd
Pavani <vemasani.pavani@gmail.com>
2024-04-25 18:42:44 +0530

    comment the code

--- module/app/main.tf
+++ module/app/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  //iam_instance_profile = aws_iam_instance_profile.instance_profile.name
   instance_market_options {
     market_type="spot"
     spot_options {
@@ -24,37 +24,37 @@ resource "aws_route53_record" "route" {
   ttl =40
 }
 
-resource "aws_iam_role" "Prometheus_role" {
-  name               = "prometheus_role"
- assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
-
-}
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = var.role
-  role = aws_iam_role.Prometheus_role.name
-
-}
-//attach policy to the prometheus roles
-resource "aws_iam_role_policy_attachment" "policy_attach" {
-  role       = aws_iam_role.Prometheus_role.name
-  policy_arn = aws_iam_policy.policy.arn
-}
-//give the permissions with the help of policy
-resource "aws_iam_policy" "policy" {
-  name        = "prometheus-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          var.policy_resource_list,
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
+//resource "aws_iam_role" "Prometheus_role" {
+//  name               = "prometheus_role"
+// assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
+//
+//}
+//resource "aws_iam_instance_profile" "instance_profile" {
+//  name = var.role
+//  role = aws_iam_role.Prometheus_role.name
+//
+//}
+////attach policy to the prometheus roles
+//resource "aws_iam_role_policy_attachment" "policy_attach" {
+//  role       = aws_iam_role.Prometheus_role.name
+//  policy_arn = aws_iam_policy.policy.arn
+//}
+////give the permissions with the help of policy
+//resource "aws_iam_policy" "policy" {
+//  name        = "policy"
+//  policy = jsonencode({
+//    Version = "2012-10-17"
+//    Statement = [
+//      {
+//        Action = [
+//          var.policy_resource_list,
+//        ]
+//        Effect   = "Allow"
+//        Resource = "*"
+//      },
+//    ]
+//  })
+//}
 
 
 
