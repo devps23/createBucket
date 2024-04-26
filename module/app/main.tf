e3446406327c70333105f84af33605ea5907438f
Pavani <vemasani.pavani@gmail.com>
2024-04-26 09:00:04 +0530

    spot instance

--- module/app/main.tf
+++ module/app/main.tf
@@ -3,13 +3,13 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
-//  instance_market_options {
-//    market_type="spot"
-//    spot_options {
-//      spot_instance_type = "persistent"
-//      instance_interruption_behavior = "stop"
-//    }
-//  }
+  instance_market_options {
+    market_type="spot"
+    spot_options {
+      spot_instance_type = "persistent"
+      instance_interruption_behavior = "stop"
+    }
+  }
   tags = {
     Name = var.tool_name
     visual = "yes"
